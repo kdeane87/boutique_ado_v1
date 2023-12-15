df3a797583c89fc245fc8e50660fcd2d3a1517eb
kdeane87 <kdeane87@gmail.com>
2023-12-15 15:33:58 +0000

    added quantity boxes to shopping bag

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -29,8 +29,8 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['8000-kdeane87-boutiqueadov1-zti8o74bvap.ws-eu106.gitpod.io']
-
+ALLOWED_HOSTS = ['8000-kdeane87-boutiqueadov1-zti8o74bvap.ws-eu107.gitpod.io']
+                  
 
 # Application definition
 
